d889d9bdf60ccc42b9712014345f5691875c9c44
YajurG <ygrover@uw.edu>
2019-11-30 10:42:10 -0600

    added basic methods

--- src/main/java/com/yajurgrover/App.java
+++ src/main/java/com/yajurgrover/App.java
@@ -1,31 +1,53 @@
 package com.yajurgrover;
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
 
 public class App 
 {
     private Map<String, Integer> groceryQuantities;
-    private Map<String, Float> groceryPrices; 
+    private Map<String, Double> groceryPrices; 
     private ArrayList<String> groceries;
+    private Integer numOfPeople;
 
-    public App(ArrayList<String> groceries, Map<String, Float> groceryPrices)
+    public App(ArrayList<String> groceries, Map<String, Double> groceryPrices, Integer numPeople)
     {
         this.groceries = groceries;
         this.groceryPrices = groceryPrices;
+        this.groceryQuantities = updateGroceryQuantities();
+        this.numOfPeople = numPeople;
     }
 
     // updates groceryQuantities to show how many of each item was bought
-    public void getGroceryQuantities() {
+    public Map<String, Integer> updateGroceryQuantities() {
+        Map<String, Integer> result = new TreeMap<String, Integer>();
         for (String item : groceries){
-            if (this.groceryQuantities.containsKey(item)){
-                Integer q = this.groceryQuantities.get(item);
+            if (result.containsKey(item)){
+                Integer q = result.get(item);
                 q++;
-                this.groceryQuantities.put(item, q);
+                result.put(item, q);
             } else {
-                this.groceryQuantities.put(item, 1);
+                result.put(item, 1);
             }
         }
+        return result;
+    }
+
+    public double getTotalPrice() {
+        double totalPrice = 0.0;
+        for (String item : this.groceryQuantities.keySet()){
+            double itemPrice = this.groceryPrices.get(item);
+            Integer itemQuantity = this.groceryQuantities.get(item);
+            totalPrice += itemPrice * itemQuantity;
+        }
+        return totalPrice;
+    }
+
+    public Integer getItemQuantity(String item){
+        return this.groceryQuantities.get(item);
+    }
+
+    public double getItemPrice(String item){
+        return this.groceryPrices.get(item);
     }
 
 
